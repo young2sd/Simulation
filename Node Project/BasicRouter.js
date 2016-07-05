211a918bc640d41219b4ad426b7b889e4e4b8c89
young2sd <young2sd@dukes.jmu.edu>
2016-07-05 16:39:02 -0400

    Unknown, 3rd party style, index
    Added the default 404 page, bootstrap, jquery

--- Node Project/BasicRouter.js
+++ Node Project/BasicRouter.js
@@ -1,9 +1,26 @@
-var express = require("express");
+//Import statements
+var express = require('express');
+var path = require('path');
 
 var app = express();
 
-app.listen(8080, handle);
+var port = 8080;
 
-function handle(request, response) {
-  reponse.write("Hello world");
+app.get("/", handleRoot);
+app.get("/index.html", handleRoot);
+app.use('/css', express.static( path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.get("*", handleUnknown);
+
+app.listen(port, function() {
+  console.log("Express function running.");
+  console.log("Listening on port " + port);
+});
+
+function handleRoot(request, response) {
+  response.sendFile(path.join(__dirname, "index.html"));
+}
+
+function handleUnknown(request, response) {
+  response.sendFile(path.join(__dirname , "unknown.html"));
 }
